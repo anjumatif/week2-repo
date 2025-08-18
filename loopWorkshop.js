f76f4c371f3b0e3d9e0e5acfb7390ad37f6919be
anjumatif <shaikhanjum4221@gmail.com>
2025-08-18 16:56:37 +0100

    for loop

--- loopWorkshop.js
+++ loopWorkshop.js
@@ -4,9 +4,16 @@ console.log("Week2 Loop workshop");
 // {
 //     console.log("Iteration",i);
 // }
- let counter=1;
-while(counter <=5)
-{
-    console.log(`counter ${counter}`);
-    counter++;
+//  let counter=1;
+// while(counter <=5)
+// {
+//     console.log(`counter ${counter}`);
+//     counter++;
+// }
+
+let food= ["pizza","buurger","chips","chocolates","pastries"];
+for(let i=0;i < food.length; i++)
+    {
+console.log(food[i]);
 }
+
